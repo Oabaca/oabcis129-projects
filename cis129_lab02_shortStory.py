4668725c39c459f0657ccacca5f6e4133227ccf1
Oabaca <159568162+Oabaca@users.noreply.github.com>
2024-04-04 09:21:44 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print('Hello, World!')
+#Story by Oscar A. Baca
+#1st CIS Lab
+#Deleted "Hello World" and wrote a story.
+print(Once upon a time there was monkey named Donkey who sang like a honkey. One day his Nana, who called herself Jana, told him that in her will she would leave him a banana. But he was so sad to think of such drama that he decided go and visit his grandma. And they had a swell time.)
